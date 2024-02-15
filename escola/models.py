c755e863d5520ae799aa6c93ebb462b0731f7595
Mach1r0 <segundacontafd@gmail.com>
2024-02-15 00:03:36 -0300

    Creating matricula for aluno and making serializer and views

--- escola/models.py
+++ escola/models.py
@@ -17,7 +17,17 @@ class Curso(models.Model):
     )
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
-    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, default='B')
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False,default='B')
 
     def __str__(self):
-        return self.descricao
+        return self.descricao 
+    
+class Matricula(models.Model):
+    PERIODO = (
+        ('M ', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=3,choices=PERIODO, blank=False, null=False,default='M')
--- escola/serializer.py
+++ escola/serializer.py
@@ -1,12 +1,17 @@
-from rest_framework import serializers 
-from escola.models import Aluno, Curso
+from rest_framework import serializers
+from .models import Aluno, Curso, Matricula
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Aluno 
-        field =  ['id', 'nome', 'rg', 'cpf']
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
-        field = '__all__'
+        fields = '__all__'  
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
